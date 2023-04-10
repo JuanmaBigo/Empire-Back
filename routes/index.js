5691667ce1ffb5b0438f1e937dcffbb741069970
facundo rodriguez <facundo2punto0@gmail.com>
2023-04-10 10:41:48 -0300

    get foto de el auto con color y llantas done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRouter from './users.js'
 import carRouter from "./cars.js"
+import rimRouter from "./rims.js"
 
 
 let router = express.Router();
@@ -10,15 +11,13 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Empire Backend' });
 });
 
-router.use("/cars",carRouter)
-
 // en el enrutador principal voy a enrutar todos los recursos
 // es decir voy a llamar y configurar las rutas de usuarios, autos, etc
 // a traves del metodo .use() le indico al enrutador principal que utilice esas rutas con el endpoint correspondiente
 
 router.use("/cars",carRouter)
 router.use("/users",userRouter)
-
+router.use("/rims",rimRouter)
 
 
 
