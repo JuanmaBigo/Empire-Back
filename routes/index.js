ed2d29436a5ad2122efaa6c1532fdb5c6ea8c08a
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-04-10 16:17:01 -0300

    merge

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,6 @@ router.get('/', function (req, res, next) {
 // a traves del metodo .use() le indico al enrutador principal que utilice esas rutas con el endpoint correspondiente
 
 router.use("/cars",carRouter)
-router.use("/users",userRouter)
 router.use("/rims",rimRouter)
 router.use("/auth",userRouter)
 router.use("/colors",colorRouter)
