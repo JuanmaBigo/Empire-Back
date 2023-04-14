0ede2a45eb085355021deadcf1fbca6c3509d263
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-04-14 18:48:33 -0300

    controlador para traer una foto pasando un id por params

--- routes/rims.js
+++ routes/rims.js
@@ -1,14 +1,18 @@
 import express from "express"
 import getPhoto from "../controllers/rims/get_photo.js"
 import getRims from "../controllers/rims/get_rims.js"
+import getCarPhoto from "../controllers/rims/get_car_photo.js"
 
 const router = express.Router()
 
 const {get_photo}= getPhoto
 const{get_rims}=getRims
+const{get_car_photo}=getCarPhoto
 
 
 router.get("/",get_photo)
 router.get("/all",get_rims)
+router.get("/:id", get_car_photo)
+
 
 export default router;
